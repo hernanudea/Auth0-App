f3c6daf98e7989274516d644781d3f0ffb55b9e0
Hernan Velasquez <hernanudea@gmail.com>
2019-05-18 22:03:28 -0500

    Se agrega Aunt0, se modifica navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private authService: AuthService) {
+    this.authService.handleAuthentication();
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  salir() {
+    this.authService.logout();
+  }
+}
